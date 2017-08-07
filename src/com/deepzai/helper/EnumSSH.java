06288de8218def9c316696090813d8ec214c15b9
Jason Liu <jasonningliu@gmail.com>
2017-08-07 09:48:27 -0700

    Added SSH key strength enum and environmental variable runtime applier

---
+++ src/com/deepzai/helper/EnumSSH.java
@@ -0,0 +1,5 @@
+package com.deepzai.helper;
+
+public enum EnumSSH {
+    ID_RSA_1024, ID_RSA_2048, ID_RSA_4096;
+}
---
+++ src/com/deepzai/helper/EnvironmentMapper.java
@@ -0,0 +1,24 @@
+package com.deepzai.helper;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Map;
+
+public class EnvironmentMapper {
+    public static void set(Map<String, String> newenv) throws Exception {
+        Class[] classes = Collections.class.getDeclaredClasses();
+        Map<String, String> env = System.getenv();
+        for(Class cl : classes) {
+            if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+                Field field = cl.getDeclaredField("m");
+                field.setAccessible(true);
+                Object obj = field.get(env);
+                Map<String, String> map = (Map<String, String>) obj;
+                map.clear();
+                map.putAll(newenv);
+                System.out.println("test" + map);
+                System.out.println(System.getenv());
+            }
+        }
+    }
+}
